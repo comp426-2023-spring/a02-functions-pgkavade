f90a119be3c672c13db2b50ac67d1070b936abfd
Preethi K <89270251+pgkavade@users.noreply.github.com>
2023-05-08 18:32:13 -0400

    Update cli.js

--- cli.js
+++ cli.js
@@ -30,18 +30,18 @@ if (args.j) {
     process.exit(0);
 }
 
-const days = args.d
+const day = args.d
 
-if (days == 0) {
-  console.log("today.")
-} else if (days > 1) {
-  console.log("in " + days + " days.")
-} else {
-  console.log("tomorrow.")
-}
-
-if (data.daily.precipitation_hours[days] == 0) {
+if (data.daily.precipitation_hours[day] == 0) {
     console.log("You will not need your galoshes.");
 } else {
     console.log("You might need your galoshes.");
+}
+
+if (day == 0) {
+  console.log("today.")
+} else if (day > 1) {
+  console.log("in " + day + " days.")
+} else {
+  console.log("tomorrow.")
 }
