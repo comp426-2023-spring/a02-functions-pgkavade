b20223a185c1727385862d2322e382f8fcadb968
Preethi K <89270251+pgkavade@users.noreply.github.com>
2023-05-08 15:57:17 -0400

    Update cli.js

--- cli.js
+++ cli.js
@@ -3,3 +3,16 @@
 import minimist from 'minimist';
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
+
+const args = minimist(process.argv.slice(2));
+
+if (args.h) {
+    console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
+    console.log("    -h            Show this help message and exit.");
+    console.log("    -n, -s        Latitude: N positive; S negative.");
+    console.log("    -e, -w        Longitude: E positive; W negative.");
+    console.log("    -z            Time zone: uses tz.guess() from moment-timezone by default.");
+    console.log("    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
+    console.log("    -j            Echo pretty JSON from open-meteo API and exit.");
+    process.exit(0);
+}
